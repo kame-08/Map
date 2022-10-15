3bb06bc8c705ab69802dcda065244fcdf4217d4f
Kame2021 <21cm0108@jec.ac.jp>
2022-10-15 23:46:20 +0900

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by Ryo on 2022/10/15.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
